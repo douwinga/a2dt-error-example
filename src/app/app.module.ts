fc157aa85de6f13413d8444241d8f3370552546e
David Ouwinga <douwinga@bethany.org>
2016-09-28 12:27:06 -0400

    Added example import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Angular2DataTableModule } from 'angular2-data-table';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    Angular2DataTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
